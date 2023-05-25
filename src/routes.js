989a1d7339804ea1d0943d650cd8b9aa411b36a1
Ibrahimyunel <ibrahim.cbshp@gmail.com>
2023-05-25 13:22:46 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addBook,
+  getAllBooks,
+  getBook,
+  updateBook,
+  deleteBook,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBook,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooks,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBook,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: updateBook,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBook,
+  },
+];
+
+module.exports = routes;
